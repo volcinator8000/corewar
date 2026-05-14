83fd03d6ed9fe0ba22a9e4ddddd96379fdb26b88
Khalil Almwakeh <khalil.almwakeh@epitech.eu>
2026-05-14 21:35:11 +0200

    fix?

--- src/execute.c
+++ src/execute.c
@@ -36,19 +36,16 @@ static int type_to_size(int type, int op_idx)
     return is_short ? IND_SIZE : DIR_SIZE;
 }
 
-static int parse_coding_byte(int cb, int *types, int nb_args)
+static void parse_coding_byte(int cb, int *types, int nb_args)
 {
     int shift = 6;
     int i = 0;
 
     while (i < nb_args) {
         types[i] = raw_to_type((cb >> shift) & 0x3);
-        if (types[i] == 0)
-            return ERR_CODE;
         shift -= 2;
         i++;
     }
-    return SUCCESS;
 }
 
 static int validate_types(int op_idx, int *types, int nb_args)
@@ -85,14 +82,13 @@ static void do_execute_process(vm_t *vm, process_t *proc, int op_idx)
     int types[MAX_ARGS_NUMBER];
     int cb;
     int nb_args;
-    int skip;
 
     nb_args = op_tab[op_idx].nbr_args;
     cb = vm->arena[(proc->pc + 1) % MEM_SIZE];
-    if (parse_coding_byte(cb, types, nb_args) != SUCCESS
-        || validate_types(op_idx, types, nb_args) != SUCCESS) {
-        skip = compute_skip(types, nb_args, op_idx);
-        proc->pc = (proc->pc + skip) % MEM_SIZE;
+    parse_coding_byte(cb, types, nb_args);
+    if (validate_types(op_idx, types, nb_args) != SUCCESS) {
+        proc->pc = (proc->pc + compute_skip(types, nb_args, op_idx))
+            % MEM_SIZE;
         return;
     }
     OP_FUNCS[op_idx](vm, proc);
